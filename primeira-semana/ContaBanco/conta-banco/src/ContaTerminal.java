c67efea45e4061844634a42e094a44a9d6651baf
Abimael Brito <160194472+abimas59@users.noreply.github.com>
2024-03-31 10:55:35 -0300

    ENtrega projeto 01

--- primeira-semana/ContaBanco/conta-banco/src/ContaTerminal.java
+++ primeira-semana/ContaBanco/conta-banco/src/ContaTerminal.java
@@ -16,8 +16,9 @@ public class ContaTerminal {
         System.out.println("Digite o valor para depósito: ");
         saldo = t.nextDouble();
 
-        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco,sua agência é: "+agencia+", conta: "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco,sua agência é: "
+        +agencia+", conta: "+numero+" e seu saldo "+saldo+" já está disponível para saque");
         
-        
+        t.close();
     }
 }
