887c2b99851d93c1777233108dcb2e8a4aca6268
Abimael Brito <160194472+abimas59@users.noreply.github.com>
2024-04-01 20:42:51 -0300

    desafio-contador

--- DesafioControleFluxo/Desafio-controle-Fluxo/src/Contador.java
+++ DesafioControleFluxo/Desafio-controle-Fluxo/src/Contador.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner t = new Scanner(System.in);
-
+        System.out.println("Digite o primeiro número: ");
         int parametroUm = t.nextInt();
+        System.out.println("Digite o segundo número: ");
         int parametroDois = t.nextInt();
 
         
@@ -14,24 +15,30 @@ public class Contador {
             // TODO Auto-generated catch block
             if (parametroUm>parametroDois){
                 
-                System.out.println("O número 2 deve ser menor que o numero 1");
+                System.out.println("O segundo número deve ser maior que o primeiro");
             }
         }
-        
+        t.close();
 
 
    }
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
-        if (parametroUm<parametroDois){
+        if (parametroDois<parametroUm) {
+            throw  new ParametrosInvalidosException();
+            
+        }
+        else{
+    
             int contagem = parametroDois-parametroUm;
+            System.out.println("Contando");
             for (int x = 0; x < contagem+1; x++)
             System.out.println(x);
-        }else{
+        }
             //System.out.println("O numero 2 deve ser menor que o numero 1");
             
         }
 
 
 
-    }
+    
 }
