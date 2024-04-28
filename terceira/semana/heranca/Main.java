8d03dbcb977b274b7d823c5ce0a36196b2716c5a
abimas59 <britoabimael12@gmail.com>
2024-04-28 17:39:44 -0300

    intellij

--- terceira/semana/heranca/Main.java
+++ terceira/semana/heranca/Main.java
@@ -2,6 +2,8 @@ package terceira.semana.heranca;
 
 public class Main {
     public static void main(String[] args) {
+        Motorista m = new Motorista();
+        Vendedor v = new Vendedor();
 
     }
 }
