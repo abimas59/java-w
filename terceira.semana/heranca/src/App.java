90605ce036a010a2d369a0aaef15ec8af13980b6
abimas59 <britoabimael12@gmail.com>
2024-05-10 14:57:03 -0300

    Atualizações

--- terceira.semana/heranca/src/App.java
+++ terceira.semana/heranca/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Vendedor v = new Vendedor();
+        Motorista m = new Motorista();
     }
 }
---
+++ terceira.semana/heranca/src/Pessoa.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Pessoa {
+    
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private float salario;
+
+}
